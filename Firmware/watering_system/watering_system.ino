00291f1345e6fcd8bc8089047425966ca5187ea2
MarCordero <marcko_385@hotmail.com>
2023-04-23 03:35:59 -0500

    Firmware update
    Update of firmware code. Work still in progress

--- Firmware/watering_system/watering_system.ino
+++ Firmware/watering_system/watering_system.ino
@@ -8,25 +8,83 @@
  *  - IS727366 Rodríguez Castro, Carlos Eduardo 
 */
 
-int potValue = 0;
-int potpot = 0;
+// Libraries section
+// #include <WiFi.h>
+// extern "C" {
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/timers.h"
+// }
+// #include <AsyncMqttClient.h>
+#include <NewPing.h>
+
+// /* Constant definition */
+// // Wi-Fi
+// #define WIFI_SSID "IoT_Network" // Local network (change to own)
+// #define WIFI_PASSWORD "IoT_P455W0RD" // Local network password (change to own)
+
+// Ultrasonic sensor
+#define TRIGGER_PIN 22
+#define ECHO_PIN 23
+#define MAX_DISTANCE 500
+
+// Humidity sensor
+#define WET_STATE 1600
+#define IDEAL_STATE 2300
+#define DRY_STATE 3000
+#define HUMIDITY_SENSOR_PIN 15
+
+// Water pump
+#define WATER_PUMP_PIN 32 // Relay pin
+
+// General
+#define BAUD_RATE 115200
+
+bool out = true;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(2, INPUT);
-  // pinMode(15, INPUT);
+  Serial.begin(BAUD_RATE);
+  pinMode(HUMIDITY_SENSOR_PIN, INPUT);
+  pinMode(WATER_PUMP_PIN, OUTPUT);
+}
+
+void waterPump() {
+
+}
+
+void soilHumidityCapture() {
+  int humidity_value = analogRead(HUMIDITY_SENSOR_PIN);
+
+  /*
+    TODO:
+      - Process read value to send MQTT message
+      - Add intervals (<=1600 soaking wet, 2300 ideal value, >=3000 dry)
+  */
+}
+
+void waterLevelCapture() {
+  // Take water level via ultrasonic sensor
+  // TODO: Implement predefined water container values
+
+  unsigned long capture_avg = 0;
+
+  for (int i = 0; i < 20; i++) {
+    capture_avg += sonar.ping_cm();
+    delay(75);
+  }
+  
+  capture_avg /= 20.0;
+  Serial.println("Sonar sensor avg: " + String(capture_avg) + "cm");
 }
 
 void loop() {
-  delay(750); // Espera de 3/4 de segundo)
+  // out = !out;
+  // digitalWrite(HUMIDITY_SENSOR_PIN, (out) ? 1 : 0);
 
-  potpot = analogRead(2);
-  potValue = map(potpot, 0, 1023, 255, 0);
-  Serial.print("Analog: ");
-  Serial.println(potpot);
-  Serial.println(potValue);
+  // Serial.print("Value: ");
+  // Serial.println((out) ? 1 : 0);
 
-  // potValue = analogRead(15); // Lectura de  valor análogo
-  // Serial.print("Analog: ");
-  // Serial.println(potValue);
+  delay(2000);
+  // Serial.println("Sensor: " + String(analogRead(HUMIDITY_SENSOR_PIN)));
+  waterLevelCapture();
 }
