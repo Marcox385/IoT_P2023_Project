f8f4fa7b9f9ec4dcbde093d2451c48a98b0bf933
MarCordero <marcko_385@hotmail.com>
2023-04-24 17:10:49 -0500

    Firmware testing cycle
    Added all components of firmware; current status of firmware: testing (pending integration)

--- Firmware/watering_system/watering_system.ino
+++ Firmware/watering_system/watering_system.ino
@@ -9,18 +9,31 @@
 */
 
 // Libraries section
-// #include <WiFi.h>
-// extern "C" {
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/timers.h"
-// }
-// #include <AsyncMqttClient.h>
+#include <WiFi.h>
+extern "C" {
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
 #include <NewPing.h>
 
-// /* Constant definition */
-// // Wi-Fi
-// #define WIFI_SSID "IoT_Network" // Local network (change to own)
-// #define WIFI_PASSWORD "IoT_P455W0RD" // Local network password (change to own)
+/* Constant definition */
+// Wi-Fi
+#define WIFI_SSID "IoT_Network" // Local network (change to own)
+#define WIFI_PASSWORD "IoT_P455W0RD" // Local network password (change to own)
+
+// MQTT
+#define MQTT_HOST "iotmadnessproject.hopto.org"
+#define MQTT_PORT 1883
+
+#define MQTT_USERNAME_PUB "publisher"
+#define MQTT_PASSWORD_PUB "publisher"
+
+#define MQTT_USERNAME_SUB "subscriber"
+#define MQTT_PASSWORD_SUB "subscriber"
+
+#define MQTT_TEST_TOPIC_PUB "TESTING_PUB"
+#define MQTT_TEST_TOPIC_SUB "TESTING_SUB"
 
 // Ultrasonic sensor
 #define TRIGGER_PIN 22
@@ -42,10 +55,137 @@
 bool out = true;
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0);  // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+  // ESP subscribed to test topic
+  uint16_t packetIdSub = mqttClient.subscribe(MQTT_TEST_TOPIC_SUB, 0);
+  Serial.println("Subscribing at QoS 0");
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  String messageTemp;
+  for (int i = 0; i < len; i++) {
+    messageTemp += (char)payload[i];
+  }
+  // Check if the MQTT message was received on topic test
+  if (strcmp(topic, MQTT_TEST_TOPIC_SUB) == 0) {
+    Serial.println("TRUE");
+  }
+
+  // if (messageTemp == "TRUE") digitalWrite(LED, HIGH);
+  // else if (messageTemp == "FALSE") digitalWrite(LED, LOW);
+
+  Serial.println("Publish received.");
+  Serial.print("  message: ");
+  Serial.println(messageTemp);
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
 void setup() {
   Serial.begin(BAUD_RATE);
+
+  // mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  // wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  // WiFi.onEvent(WiFiEvent);
+
+  // // MQTT Setup
+  // mqttClient.onConnect(onMqttConnect);
+  // mqttClient.onDisconnect(onMqttDisconnect);
+
+  // // MQTT Publish
+  // mqttClient.onPublish(onMqttPublish);
+
+  // // MQTT Subscribe
+  // mqttClient.onSubscribe(onMqttSubscribe);
+  // mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  // mqttClient.onMessage(onMqttMessage);
+
+  // // MQTT Connection
+  // mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  // mqttClient.setCredentials(MQTT_USERNAME_SUB, MQTT_PASSWORD_SUB);
+  // connectToWifi();
+
   pinMode(HUMIDITY_SENSOR_PIN, INPUT);
   pinMode(WATER_PUMP_PIN, OUTPUT);
+
+  pinMode(2, INPUT);
+
+  digitalWrite(WATER_PUMP_PIN, 1);
 }
 
 void waterPump() {
@@ -53,7 +193,12 @@ void waterPump() {
 }
 
 void soilHumidityCapture() {
-  int humidity_value = analogRead(HUMIDITY_SENSOR_PIN);
+  unsigned long capture_avg = 0;
+
+  for (int i = 0; i < 20; i++) {
+    capture_avg += analogRead(HUMIDITY_SENSOR_PIN);
+    delay(65);
+  }
 
   /*
     TODO:
@@ -77,6 +222,11 @@ void waterLevelCapture() {
   Serial.println("Sonar sensor avg: " + String(capture_avg) + "cm");
 }
 
+void test_publish(char* msg) {
+  uint16_t packetIdPub1 = mqttClient.publish(MQTT_TEST_TOPIC_PUB, 1, true, String(msg).c_str());
+  Serial.printf("Publishing on topic %s at QoS 1, packetId: %i; Message: ", MQTT_TEST_TOPIC_PUB, packetIdPub1);
+}
+
 void loop() {
   // out = !out;
   // digitalWrite(HUMIDITY_SENSOR_PIN, (out) ? 1 : 0);
@@ -84,7 +234,11 @@ void loop() {
   // Serial.print("Value: ");
   // Serial.println((out) ? 1 : 0);
 
-  delay(2000);
   // Serial.println("Sensor: " + String(analogRead(HUMIDITY_SENSOR_PIN)));
-  waterLevelCapture();
+  // waterLevelCapture();
+
+  
+
+  delay(500);
+  digitalWrite(WATER_PUMP_PIN, (digitalRead(4) == 1) ? 0 : 1);
 }
